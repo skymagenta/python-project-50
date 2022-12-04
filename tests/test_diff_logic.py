4e88a1b452c7a646a1d23a601c323d798184d59c
skymagenta <jokero.sky1@gmail.com>
2022-12-04 23:23:56 +0300

    fix tests

--- tests/test_diff_logic.py
+++ tests/test_diff_logic.py
@@ -1,54 +1,52 @@
-# from gendiff.diff_logic import generate_diff
-# from gendiff.formatter import get_stylish
-from gendiff.diff_logic import get_dict, get_diff
-from gendiff.formatters.stylish import get_stylish
-from gendiff.formatters.plain import get_plain
-from gendiff.formatters.json import get_json
+import pytest
+
+from gendiff.generate_diff import generate_diff
+
+JSON1 = 'tests/fixtures/file1.json'
+JSON2 = 'tests/fixtures/file2.json'
+JSON1_NESTED = 'tests/fixtures/file11.json'
+JSON2_NESTED = 'tests/fixtures/file22.json'
+YML1 = 'tests/fixtures/file1.yml'
+YML2 = 'tests/fixtures/file2.yml'
+YML1_NESTED = 'tests/fixtures/file11.yml'
+YML2_NESTED = 'tests/fixtures/file22.yml'
+YAML1 = 'tests/fixtures/file1.yaml'
+YAML2 = 'tests/fixtures/file2.yaml'
+YAML1_NESTED = 'tests/fixtures/file11.yaml'
+YAML2_NESTED = 'tests/fixtures/file22.yaml'
+
+EXPECTED_STYLISH = 'tests/fixtures/stylish.txt'
+EXPECTED_STYLISH_NESTED = 'tests/fixtures/stylish_nested.txt'
+EXPECTED_PLAIN = 'tests/fixtures/plain.txt'
+EXPECTED_PLAIN_NESTED = 'tests/fixtures/plain_nested.txt'
+EXPECTED_JSON = 'tests/fixtures/json.txt'
+EXPECTED_JSON_NESTED = 'tests/fixtures/json_nested.txt'
 
 
-def test_get_stylish_json():
-    with open('tests/fixtures/expected1.txt') as file:
-        expected_result_1 = file.read()
-    with open('tests/fixtures/expected2.txt') as file:
-        expected_result_2 = file.read()
-    with open('tests/fixtures/expected3.txt') as file:
-        expected_result_3 = file.read()
-    with open('tests/fixtures/expected4.txt') as file:
-        expected_result_4 = file.read()
-    
-    file_path1 = 'tests/fixtures/file1.json'
-    file_path2 = 'tests/fixtures/file2.json'
-    file_path3 = 'tests/fixtures/file11.json'
-    file_path4 = 'tests/fixtures/file22.json'
-    result_1 = get_stylish(get_diff(*get_dict(file_path1, file_path2)))
-    result_2 = get_stylish(get_diff(*get_dict(file_path3, file_path4)))
-    result_3 = get_plain(get_diff(*get_dict(file_path3, file_path4)))
-    result_4 = get_json(get_diff(*get_dict(file_path3, file_path4)))
-    assert result_1 == expected_result_1
-    assert result_2 == expected_result_2
-    assert result_3 == expected_result_3
-    assert result_4 == expected_result_4
+@pytest.mark.parametrize('file_path1, file_path2, format, expexted_file_path', [
+    (JSON1, JSON2, 'stylish', EXPECTED_STYLISH),
+    (JSON1, JSON2, 'plain', EXPECTED_PLAIN),
+    (JSON1, JSON2, 'json', EXPECTED_JSON),
+    (JSON1_NESTED, JSON2_NESTED, 'stylish', EXPECTED_STYLISH_NESTED),
+    (JSON1_NESTED, JSON2_NESTED, 'plain', EXPECTED_PLAIN_NESTED),
+    (JSON1_NESTED, JSON2_NESTED, 'json', EXPECTED_JSON_NESTED),
 
+    (YML1, YML2, 'stylish', EXPECTED_STYLISH),
+    (YML1, YML2, 'plain', EXPECTED_PLAIN),
+    (YML1, YML2, 'json', EXPECTED_JSON),
+    (YML1_NESTED, YML2_NESTED, 'stylish', EXPECTED_STYLISH_NESTED),
+    (YML1_NESTED, YML2_NESTED, 'plain', EXPECTED_PLAIN_NESTED),
+    (YML1_NESTED, YML2_NESTED, 'json', EXPECTED_JSON_NESTED),
 
-def test_get_stylish_yaml():
-    with open('tests/fixtures/expected1.txt') as file:
-        expected_result_1 = file.read()
-    with open('tests/fixtures/expected2.txt') as file:
-        expected_result_2 = file.read()
-    with open('tests/fixtures/expected3.txt') as file:
-        expected_result_3 = file.read()
-    with open('tests/fixtures/expected4.txt') as file:
-        expected_result_4 = file.read()
-    
-    file_path1 = 'tests/fixtures/file1.yml'
-    file_path2 = 'tests/fixtures/file2.yml'
-    file_path3 = 'tests/fixtures/file11.yml'
-    file_path4 = 'tests/fixtures/file22.yml'
-    result_1 = get_stylish(get_diff(*get_dict(file_path1, file_path2)))
-    result_2 = get_stylish(get_diff(*get_dict(file_path3, file_path4)))
-    result_3 = get_plain(get_diff(*get_dict(file_path3, file_path4)))
-    result_4 = get_json(get_diff(*get_dict(file_path3, file_path4)))
-    assert result_1 == expected_result_1
-    assert result_2 == expected_result_2
-    assert result_3 == expected_result_3
-    assert result_4 == expected_result_4
+    (YAML1, YAML2, 'stylish', EXPECTED_STYLISH),
+    (YAML1, YAML2, 'plain', EXPECTED_PLAIN),
+    (YAML1, YAML2, 'json', EXPECTED_JSON),
+    (YAML1_NESTED, YAML2_NESTED, 'stylish', EXPECTED_STYLISH_NESTED),
+    (YAML1_NESTED, YAML2_NESTED, 'plain', EXPECTED_PLAIN_NESTED),
+    (YAML1_NESTED, YAML2_NESTED, 'json', EXPECTED_JSON_NESTED)
+])
+
+def test_generate_diff(file_path1, file_path2, format, expexted_file_path):
+    with open(expexted_file_path) as file:
+        expected_result = file.read()
+    assert expected_result == generate_diff(file_path1, file_path2, format)
