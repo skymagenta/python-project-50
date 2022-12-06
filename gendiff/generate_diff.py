c1a3737763b0d367e2eb8f8fb42b8d0001bbe59a
skymagenta <jokero.sky1@gmail.com>
2022-12-06 23:43:35 +0300

    fix generate_diff function

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -2,5 +2,5 @@ from gendiff.formatters.visualize_diff import visualize_diff
 from gendiff.diff_logic import get_dict, get_diff
 
 
-def generate_diff(filepath1, filepath2, format):
+def generate_diff(filepath1, filepath2, format='stylish'):
     return visualize_diff(get_diff(*get_dict(filepath1, filepath2)), format)
